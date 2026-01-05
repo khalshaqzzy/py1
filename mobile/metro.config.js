1a449b73968d58b77a60e7ad7006b82825d04f2f
khalshaqzzy <19623094@std.stei.itb.ac.id>
2026-01-05 17:27:52 +0700

    fix: expo start

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,6 +1,11 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro-config");
+import { getDefaultConfig } from "expo/metro-config";
+import { withNativeWind } from "nativewind/metro-config";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./src/styles/global.css" });
+export default withNativeWind(config, { input: "./src/styles/global.css" });
