8e1c9ff46944e628b882bb50c0d0ff79e5ae4c32
khalshaqzzy <19623094@std.stei.itb.ac.id>
2026-01-05 20:31:52 +0700

    fix build

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,7 +1,14 @@
-import { getDefaultConfig } from "expo/metro-config.js";
-import { withNativeWind } from "nativewind/metro-config.js";
+import { getDefaultConfig } from "expo/metro-config";
+import { withNativeWind } from "nativewind/metro-config";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { createRequire } from 'module';
 
-const config = getDefaultConfig(process.cwd());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const require = createRequire(import.meta.url);
+
+const config = getDefaultConfig(__dirname);
 
 export default withNativeWind(config, { 
   input: "./src/styles/global.css",
