d76cee76039e428abd63828a06cfc455c930435f
khalshaqzzy <19623094@std.stei.itb.ac.id>
2026-01-05 20:11:17 +0700

    fix: expo build

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import nativewind from "nativewind/preset";
+
+export default {
   // NOTE: Update this to include the paths to all of your component files.
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  presets: [nativewind],
   theme: {
     extend: {},
   },
