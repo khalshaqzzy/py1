f9a3d9ba3e304b69ceb02fcf2e8d1f6f94eafaa3
khalshaqzzy <19623094@std.stei.itb.ac.id>
2026-01-05 20:18:43 +0700

    Update tailwind.config.js

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -2,11 +2,10 @@
 import nativewind from "nativewind/preset";
 
 export default {
-  // NOTE: Update this to include the paths to all of your component files.
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   presets: [nativewind],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
